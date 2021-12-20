26f07e13337ca456e1cb203f99bb517a811a69de
Baron Willeford <baron.willeford@gmail.com>
2021-12-20 14:43:49 -0800

    convert subscriptions to use redis

--- src/subscriptions.ts
+++ src/subscriptions.ts
@@ -1,8 +1,17 @@
-// import { RedisPubSub } from 'graphql-redis-subscriptions';
-import { PubSub } from 'graphql-subscriptions';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import Redis from 'ioredis';
 
-// export const pubsub = new RedisPubSub();
-export const pubsub = new PubSub();
+const options = {
+  port: 6379,
+  host: '0.0.0.0',
+  password: 'sOmE_sEcUrE_pAsS',
+  family: 4,
+};
+
+export const pubsub = new RedisPubSub({
+  publisher: new Redis(options),
+  subscriber: new Redis(options),
+});
 
 export const GAME_UPDATED = 'GAME_UPDATED';
 
